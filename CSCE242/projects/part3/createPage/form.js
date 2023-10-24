cbc989fff0b92ed8208ac0657611166db739e1cb
AChow1023 <andychow180@gmail.com>
2023-10-23 23:32:08 -0400

    finish

--- CSCE242/projects/part3/createPage/form.js
+++ CSCE242/projects/part3/createPage/form.js
@@ -4,7 +4,7 @@ const submitForm = (e) =>{
     document.getElementById("result").classList.remove("hidden");
     const form = document.getElementById("form");
     const uName = form.elements["username"].value;
-    const name = form.elements["name"].value;
+    const name = form.elements["name-input"].value;
     const PName = form.elements["page-name"].value;
     const type = form.elements["type"].value;
     const userContent = form.elements["user-content"].value;
